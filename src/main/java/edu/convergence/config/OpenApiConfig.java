3e9a8f9259aea56651d07fa12941811c5c79e102
nimantha1999 <nimantha.bt@gmail.com>
2026-04-26 13:35:06 +0530

    add OpenAPI configuration for Customer API and update dependencies

---
+++ src/main/java/edu/convergence/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package edu.convergence.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customerOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Customer API")
+                        .description("API for managing customers")
+                        .version("1.0.0"));
+    }
+}
